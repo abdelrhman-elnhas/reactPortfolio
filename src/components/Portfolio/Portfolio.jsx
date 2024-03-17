fe67372a1e5c97146cf50d171ad8761500c7b842
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2024-03-17 15:22:35 +0200

    'MarchUpdates'

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -71,7 +71,7 @@ const Portfolio = React.forwardRef((props, portfolioref) => {
           </BarGroup>
         </RightSide>
       </Container>
-      <Update>Last Update On February 2024</Update>
+      <Update>Last Update On March 2024</Update>
     </Section>
   );
 });
