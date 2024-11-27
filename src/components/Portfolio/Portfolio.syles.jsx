17a22139d201f083f62fd8bf61c1f27c2aa1c928
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2024-11-27 21:04:04 +0200

    test deployment error

--- src/components/Portfolio/Portfolio.syles.jsx
+++ src/components/Portfolio/Portfolio.syles.jsx
@@ -10,7 +10,7 @@ export const Section = styled.div`
   -webkit-user-select: none;
   -o-user-select: none;
   user-select: none;
-`
+`;
 
 export const Container = styled.div`
   width: 100%;
@@ -27,30 +27,28 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
   }
-`
-
+`;
 
 export const Title = styled.p`
   font-size: 50px;
-  color: ${props => props.theme.thirdColor};
+  color: ${(props) => props.theme.thirdColor};
   margin-bottom: -20px;
 
   @media (max-width: 850px) {
     font-size: 45px;
   }
-
-`
+`;
 
 export const Desc = styled.p`
   font-size: 35px;
-  color: ${props => props.theme.thirdColor};
+  color: ${(props) => props.theme.thirdColor};
   @media (max-width: 965px) {
     font-size: 30px;
   }
   @media (max-width: 850px) {
     font-size: 25px;
   }
-`
+`;
 
 export const LeftSide = styled.div`
   flex: 3;
@@ -58,7 +56,7 @@ export const LeftSide = styled.div`
   flex-direction: column;
   align-items: flex-start;
   align-self: flex-start;
-  
+
   @media (max-width: 1095px) {
     padding-left: 30px;
   }
@@ -66,14 +64,14 @@ export const LeftSide = styled.div`
     flex: unset;
     padding-left: 65px;
   }
-  @media (max-width:598px) {
+  @media (max-width: 598px) {
     padding-left: 50px;
   }
-  @media (max-width:498px) {
+  @media (max-width: 498px) {
     padding-left: 40px;
     margin-bottom: -40px;
   }
-`
+`;
 
 export const RightSide = styled.div`
   flex: 2;
@@ -81,19 +79,18 @@ export const RightSide = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
-  
+
   @media (max-width: 1095px) {
     padding-right: 30px;
-  } 
+  }
   @media (max-width: 768px) {
     flex: unset;
     width: 80%;
   }
-
-`
+`;
 
 export const Screen = styled.div`
-  background-color: ${props => props.theme.thirdColor};
+  background-color: ${(props) => props.theme.thirdColor};
   border-radius: 20px;
   -webkit-border-radius: 30px;
   -moz-border-radius: 30px;
@@ -103,8 +100,7 @@ export const Screen = styled.div`
   justify-content: center;
   align-items: center;
   aspect-ratio: 3/2;
-
-`
+`;
 
 export const ProjectImage = styled.img`
   width: 80%;
@@ -112,7 +108,7 @@ export const ProjectImage = styled.img`
   -webkit-border-radius: 30px;
   -moz-border-radius: 30px;
   -ms-border-radius: 30px;
-`
+`;
 
 export const BarGroup = styled.div`
   display: flex;
@@ -121,20 +117,20 @@ export const BarGroup = styled.div`
   justify-content: center;
   align-items: center;
   width: 90%;
-  
-`
+`;
 
 export const Bar = styled.span`
   font-size: 1.2rem;
   text-align: center;
   content: "";
-  background-color: ${ props => props.theme.mainColor};
+  background-color: ${(props) => props.theme.mainColor};
   width: 100%;
   height: 30px;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${ props => props.theme.thirdColor};
+  color: ${(props) => props.theme.thirdColor};
+  cursor: pointer;
   border-radius: 50px;
   -webkit-border-radius: 50px;
   -moz-border-radius: 50px;
@@ -148,20 +144,19 @@ export const Bar = styled.span`
   -ms-transition: all 0.5s;
   -o-transition: all 0.5s;
 
-  &:hover{
-    font-size: 1.2rem;  
+  &:hover {
+    font-size: 1.2rem;
     content: "";
-    background-color: ${ props => props.theme.thirdColor};
-    color: ${ props => props.theme.mainColor};
+    background-color: ${(props) => props.theme.thirdColor};
+    color: ${(props) => props.theme.mainColor};
     border-radius: 50px;
     -webkit-border-radius: 50px;
     -moz-border-radius: 50px;
     -ms-border-radius: 50px;
     -o-border-radius: 50px;
-    color: ${ props => props.theme.mainColor};
+    color: ${(props) => props.theme.mainColor};
   }
-`
-
+`;
 
 export const Button = styled.button`
   transition: all 0.2s ease-in;
@@ -170,17 +165,17 @@ export const Button = styled.button`
   position: relative;
   overflow: hidden;
   z-index: 1;
-  color: ${props => props.theme.thirdColor};
+  color: ${(props) => props.theme.thirdColor};
   padding: 0.7em 1.7em;
   font-size: 18px;
   border-radius: 50px;
-  background: ${props => props.theme.mainColor};
-  border: 3px solid ${props => props.theme.thirdColor};
+  background: ${(props) => props.theme.mainColor};
+  border: 3px solid ${(props) => props.theme.thirdColor};
   align-self: center;
   margin-top: 25px;
-  
+
   &:active {
-  color: ${props => props.theme.thirdColor};
+    color: ${(props) => props.theme.thirdColor};
   }
 
   &:before {
@@ -205,36 +200,36 @@ export const Button = styled.button`
     top: 180%;
     width: 160%;
     height: 190%;
-    background-color: ${props => props.theme.thirdColor};
+    background-color: ${(props) => props.theme.thirdColor};
     border-radius: 50px;
     transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
     z-index: -1;
   }
 
   &:hover {
-    color: ${props => props.theme.mainColor};
-    border: 3px solid ${props => props.theme.thirdColor};
+    color: ${(props) => props.theme.mainColor};
+    border: 3px solid ${(props) => props.theme.thirdColor};
   }
 
   &:hover:before {
     top: -35%;
-    background-color: ${props => props.theme.thirdColor};
+    background-color: ${(props) => props.theme.thirdColor};
     transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
   }
 
   &:hover:after {
     top: -45%;
-    background-color: ${props => props.theme.thirdColor};
+    background-color: ${(props) => props.theme.thirdColor};
     transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
   }
-`
+`;
 
 export const Update = styled.div`
   width: 100%;
-  background-color: ${props => props.theme.mainColor};
-  color: ${props => props.theme.thirdColor};
+  background-color: ${(props) => props.theme.mainColor};
+  color: ${(props) => props.theme.thirdColor};
   font-size: 20px;
   text-align: center;
   padding: 25px;
   margin-top: 30px;
-`
+`;
