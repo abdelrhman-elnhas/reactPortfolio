e6ca7e7dc5f50fc5ce4d8e9efbd1bda1328452d9
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2024-11-27 20:42:25 +0200

    edit: about content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,22 +16,22 @@ function About() {
         <AboutContent>
           <Title>&lt; About &gt;</Title>
           <Desc>
-            <Name>Hi There! I'm Abdelrhman Elnhas.</Name> I'm an Electrical
-            Engineering Student, Majoring in computers and control systems. I
-            have passion for all things that fall under the scope of technology,
-            programming and design. In addition to my love of technology and
-            design , I'm also interested in languages. Here you can see some of
-            my skills.
+            <Name>Hi There! I'm Abdelrhman Elnhas.</Name> I’m a passionate
+            Software Engineer with a specialization in Frontend Development.
+            Technology, programming, and design are at the heart of everything I
+            do. I thrive on transforming ideas into seamless, user-friendly
+            experiences. Here, you’ll find a showcase of my skills and
+            expertise.
           </Desc>
         </AboutContent>
         <AboutBars>
           <ProgressBar skill="React" rate="75%" />
           <ProgressBar skill="Javascript" rate="80%" />
           <ProgressBar skill="Redux Toolkit" rate="70%" />
-          <ProgressBar skill="Tailwind" rate="60%" />
+          <ProgressBar skill="Tailwind" rate="70%" />
           <ProgressBar skill="Bootstrap" rate="90%" />
           <ProgressBar skill="Styled-Components" rate="60%" />
-          <ProgressBar skill="SASS" rate="60%" />
+          <ProgressBar skill="SASS" rate="80%" />
           <ProgressBar skill="Python" rate="70%" />
           <ProgressBar skill="Laravel" rate="50%" />
           <ProgressBar skill="MySQL" rate="75%" />
