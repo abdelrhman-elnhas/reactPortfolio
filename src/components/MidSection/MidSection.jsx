985eaf4902b1de6ea34313e8ea5f06862fa06683
Abdelrhman A. Elnhas <elnhasdesigner@gmail.com>
2025-03-07 15:25:31 +0200

    Update CV March 2025

--- src/components/MidSection/MidSection.jsx
+++ src/components/MidSection/MidSection.jsx
@@ -1,24 +1,44 @@
-import {Link} from 'react-router-dom';
-import CV from '../../Files/AbdelrhmanAhmedElnhasCV2024.pdf';
-import {Section, Container , Title , Desc , ButtonGroup , Button} from './MidSection.styles';
+import { Link } from "react-router-dom";
+import CV from "../../Files/AbdelrhmanAhmedElnhas2025.pdf";
+import {
+  Section,
+  Container,
+  Title,
+  Desc,
+  ButtonGroup,
+  Button,
+} from "./MidSection.styles";
 
 function MidSection() {
   return (
     <Section>
       <Container>
         <Title>Have Any Projects in mind?</Title>
-        <Desc>Let's build a project together , I'm always honoured to work with great people.</Desc>
+        <Desc>
+          Let's build a project together , I'm always honoured to work with
+          great people.
+        </Desc>
         <ButtonGroup>
           <Link
-          to={CV}
-          download="AbdelrhmanElnhasCV"
-          target="_blank"
-          rel="noreferrer"><Button>Download CV</Button></Link>
-          <Button onClick={() => window.location = 'mailto:abdelrhman.elnhas1@gmail.com?subject=I WANT TO HIRE YOU!'}>Hire Me</Button>
+            to={CV}
+            download="AbdelrhmanElnhasCV"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>Download CV</Button>
+          </Link>
+          <Button
+            onClick={() =>
+              (window.location =
+                "mailto:abdelrhman.elnhas1@gmail.com?subject=I WANT TO HIRE YOU!")
+            }
+          >
+            Hire Me
+          </Button>
         </ButtonGroup>
       </Container>
     </Section>
-  )
+  );
 }
 
-export default MidSection
+export default MidSection;
